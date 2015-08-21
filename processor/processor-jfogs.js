71d11a86ef768be22bb499754e6e656736482b36
zswang <wjhu111@21cn.com>
2015-08-21 17:14:30 +0800

    publish version 1.0.0

--- processor/processor-jfogs.js
+++ processor/processor-jfogs.js
@@ -6,7 +6,6 @@ var jfogs = require('jfogs');
  * @param {string} content 文本内容
  * @param {Object} attrs 属性
  * @param {string} attrs.type 参数混淆方式
- * @param {string} attrs.cross 添加混淆属性
  * @param {Object} scope 作用域
  * @param {Function} scope.execImport 导入数据
  */
@@ -14,8 +13,7 @@ module.exports = function processor(content, attrs, scope) {
   if (!content) {
     return content;
   }
-  var options = {};
-  options.type = scope.execImport(attrs.type);
-  options.cross = /^(yes|on|true|ok)$/.test(scope.execImport(attrs.cross));
-  return jfogs.obfuscate(content, options);
+  return jfogs.obfuscate(content, {
+    type: scope.execImport(attrs.type)
+  });
 };
