96d45eda54239bf607fd6014f187dd92023bab39
zswang <wjhu111@21cn.com>
2015-09-16 00:01:11 +0800

    add babel test

--- processor-extend/processor-babel.js
+++ processor-extend/processor-babel.js
@@ -6,7 +6,7 @@ var babel = require('babel');
  * @param {string} content 文本内容
  * @param {Object} attrs 属性
  * @param {Object} scope 作用域
- * @param {Function} scope.getFilename 获取当前文件名
+ * @param {Function} scope.getDirname 获取当前目录名
  */
 module.exports = function processor(content, attrs, scope) {
   if (!content) {
@@ -14,6 +14,6 @@ module.exports = function processor(content, attrs, scope) {
   }
 
   return babel.transform(content, {
-    filename: scope.getFilename()
+    filename: scope.getDirname()
   }).code;
 };
