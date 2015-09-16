9ac86ea524245ffed4bd502eb4c75d02af7ad52d
zswang <wjhu111@21cn.com>
2015-09-16 08:25:21 +0800

    add attribute "compatibility" to processor "clean-css"

--- processor/processor-clean-css.js
+++ processor/processor-clean-css.js
@@ -4,7 +4,14 @@ var cleanCSS = require('clean-css');
  * 压缩 CSS
  *
  * @param {string} content 文本内容
+ * @param {Object} attrs 属性
+ * @param {Object} attrs.compatibility 兼容性
+ *     @see https://www.npmjs.com/package/clean-css#how-to-set-compatibility-mode
+ * @param {Object} scope 作用域
+ * @param {Function} scope.execImport 导入数据
  */
-module.exports = function processor(content) {
-  return new cleanCSS().minify(content).styles;
+module.exports = function processor(content, attrs, scope) {
+  return new cleanCSS({
+    compatibility: scope.execImport(attrs.compatibility)
+  }).minify(content).styles;
 };
