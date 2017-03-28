98a79afe95b690db1cccf9518bb3c8721f9b1470
zswang <wangjihu@sensorsdata.cn>
2017-03-28 17:48:50 +0800

    ✨ Add "Template Strings" processor.

---
+++ processor-extend/processor-templatestrings.js
@@ -0,0 +1,38 @@
+/**
+ * ES6 模版字符串转化为 ES5- 字符串表达式
+ *
+ * @param {string} content 文本内容
+ */
+module.exports = function (content) {
+  return content.replace(/`((\\.|[^])*?)`/g, function (all, text) {
+    function encode(str) {
+      return str.replace(/(\\.|[\n\r"])/g, function (char) {
+        if (char.length === 2) {
+          return char;
+        }
+        return {
+          '\n': '\\n',
+          '\r': '\\r',
+          '"': '\\"'
+        }[char];
+        switch (char) {
+          case '\n':
+            return '\\n';
+          case '\r':
+            return '\\r';
+          case '"':
+            return '\\"';
+        }
+      });
+    }
+    var result = '"';
+    var reg = /\$\{([^]*?)\}/;
+    var match = reg.exec(text);
+    while (match) {
+      text = RegExp["$'"];
+      result += encode(RegExp['$`']) + '" + (' + match[1] + ') + "';
+      match = reg.exec(text);
+    }
+    return result + encode(text) + '"';
+  });
+}；
